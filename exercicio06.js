7a1afbb82c283614b16516e6966a3e9274f3faa1
Gabriel Neves DE Lazari <gabriel.lazari@aluno.senai.br>
2024-04-25 11:13:47 -0300

    \z\z\

--- exercicio06.js
+++ exercicio06.js
@@ -5,6 +5,10 @@ console.log("------------------------------");
 console.log("Lista de todos os alunos");
 console.log("------------------------------");
 
-const aluno =  dados.pesquisarAlunoNotas("Gabriel")
-
-console.log(aluno);
+const aluno =  dados.pesquisarAlunoNotas("Miguel")
+if(aluno){
+    console.log(aluno);
+}
+else{
+    console.log("Aluno não encontrado na lista")
+}
