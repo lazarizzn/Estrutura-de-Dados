f4627b9008d872cb37a3156ae1a08aa412b41d97
Gabriel Neves DE Lazari <gabriel.lazari@aluno.senai.br>
2024-04-25 10:20:37 -0300

    c

---
+++ exercicio05.js
@@ -0,0 +1,33 @@
+const dados = require('./dados');
+
+console.log("Exercicio 04");
+console.log("------------------------");
+console.log("Lista de todos os alunos");
+console.log("------------------------");
+
+
+for(const aluno of dados.alunoNotas){
+
+    console.log("------------------------");
+    console.log(aluno.nome+" - "+aluno.notas);
+
+
+
+
+
+let Medianotas = 0;
+
+for(let i = 0; i < aluno.notas.length; i++){
+    Medianotas = Medianotas + aluno.notas[i];
+} 
+if(Medianotas / aluno.notas.length >=8){
+    console.log("O aluno atingio a Media: ", Medianotas / aluno.notas.length)
+}
+else{
+    console.log("Não atingio a media")
+}
+
+
+
+
+}
